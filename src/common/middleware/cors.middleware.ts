ed7db6e7c0e0f4660303a04de31f5d5a20c297e8
王小泽 <wangxiaoze@petalmail.com>
2025-01-16 19:05:56 +0800

    feat: cors中间件

---
+++ src/common/middleware/cors.middleware.ts
@@ -0,0 +1,19 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class CorsMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const allowedOrigin = process.env.CORS_ORIGIN;
+    res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Content-Type,Authorization, Content-Length, X-Requested-With',
+    );
+    next();
+  }
+}
